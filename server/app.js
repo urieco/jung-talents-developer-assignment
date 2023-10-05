e12a932982e63b99b61b7e9d30a98a475c6bd7df
Do Dang Huy <huy.dodang27@gmail.com>
2023-10-05 11:42:01 +0700

    Set up some build scripts

--- server/app.js
+++ server/app.js
@@ -1,5 +1,22 @@
-import http from 'http';
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import open from 'open';
 
-http.createServer((req, res) => {
-  res.end('hello');
-}).listen(8080);
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const { NODE_ENV } = process.env;
+const app = express();
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.listen(PORT, async () => {
+  if (NODE_ENV === 'production') {
+    await open(`http://localhost:${PORT}`);
+  }
+  console.log(`Server is running on port ${PORT}`);
+});
